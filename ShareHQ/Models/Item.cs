0c9b7e03222d7480f49af653c306cfea5d56b58c
gimunaretto <gimunaretto@gmail.com>
2021-09-24 21:38:26 -0300

    Criação das classes Categoria / Item / Usuario

---
+++ ShareHQ/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareHQ.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public int UsuarioId { get; set; }
+        public string Titulo { get; set; }
+        public string NrEdicao { get; set; }
+        public string Descricao { get; set; }
+        public Categoria Categoria { get; set; }
+        public int CategoriaId { get; set; }
+        public bool? Disponivel { get; set; }
+
+    }
+}
---
+++ ShareHQ/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareHQ.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NomeUsuario { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
